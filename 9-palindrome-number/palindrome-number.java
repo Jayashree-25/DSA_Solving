210c80034c3dc4e8a34d5e8efa14af7208031c2b
Jayashree Das <jayashree.xlite@gmail.com>
2025-09-26 23:05:44 +0530

    Time: 5 ms (65.28%) | Memory: 43.8 MB (89.18%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int org = x;
+        int reverse = 0;
+        if(x<0) return false;
+        while(x>0){
+            int digit = x%10;
+            reverse = reverse * 10 + digit;
+            x = x/10;
+        }
+        if(org == reverse){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
