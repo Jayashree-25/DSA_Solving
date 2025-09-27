0fd86a288f5876a3fcbd8d16bd3d2524b6288aa0
Jayashree Das <jayashree.xlite@gmail.com>
2025-09-27 10:12:55 +0530

    Time: 3 ms (90.70%) | Memory: 44.4 MB (56.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,11 +3,11 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        
+
         char[] schar = s.toCharArray();
-        Arrays.sort(schar);
         char[] tchar = t.toCharArray();
         Arrays.sort(tchar);
+        Arrays.sort(schar);
         
         return Arrays.equals(schar, tchar);
     }
