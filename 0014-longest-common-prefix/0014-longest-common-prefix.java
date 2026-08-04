29210000fa49cbda56260c1e4e97a237e4fcb275
Jayashree Das <jayashree.xlite@gmail.com>
2026-08-04 14:46:43 +0530

    Time: 1 ms (61.3%), Space: 43.3 MB (32.81%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -8,6 +8,9 @@ class Solution {
                 j++;
             }
             prefix = prefix.substring(0, j);
+            if (prefix.isEmpty()) {
+                return "";
+            }
         }
         return prefix;
     }
